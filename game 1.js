080abce9b2f2f05379cc2c48e74b56a3b850e2a2
MustafaAslan93 <mustafaaslan93@gmail.com>
2016-04-15 01:39:02 +0300

    practice update game 1 - game 1.js and js.cookie

---
+++ game 1.js
@@ -0,0 +1,13 @@
+function Numbers() {
+    this.numCorrect = 0;
+    this.number = [];
+    //initialize array with standard numbers
+};
+function Numbers(cookie) {
+    //read cookie
+    //initialize array
+};
+function Number(word,digit,errorNum,placed) {
+    this.word = word;
+    //continue here
+}
