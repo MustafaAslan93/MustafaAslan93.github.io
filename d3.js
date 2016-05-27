e813903b848d3f8a0331614306185583040551cd
MustafaAslan93 <mustafaaslan93@gmail.com>
2016-05-27 14:04:02 +0300

    website example new

---
+++ d3.js
@@ -0,0 +1,10 @@
+{
+     "name": "Bioinformatics", "size": 200000, "link": "http://www.ncbi.nlm.nih.gov/guide/all/",
+     "children": [
+      {"name": "Genomics", "size": 200000, "link": "http://www.swissregulon.unibas.ch/fcgi/sr"},
+      {"name": "Proteomics", "size": 200000, "link": "http://www.uniprot.org/"},
+      {"name": "Transcriptomics", "size": 200000, "link": "http://bgee.org/"},
+      {"name": "Drug Design", "size": 200000, "link": "http://www.swissbioisostere.ch/"},
+      {"name": "Structural", "size": 200000, "link": "http://www.proteinmodelportal.org/"}
+     ]
+ }
